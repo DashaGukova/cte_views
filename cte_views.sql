14e7b6a3dcc7e876265f21f5ddb59307fc4a5864
DashaGukova <66441988+DashaGukova@users.noreply.github.com>
2021-03-26 15:28:49 +0200

    Update cte_views.sql

--- cte_views.sql
+++ cte_views.sql
@@ -1,16 +1,23 @@
 USE AdventureWorks2019
 GO
 CREATE VIEW Person.vPerson AS 
-SELECT Title, FirstName, LastName, EmailAddress 
-FROM Person.Person, Person.EmailAddress
+SELECT p.Title, p.FirstName, p.LastName, e.EmailAddress 
+FROM Person.Person AS p 
+JOIN Person.EmailAddress AS e 
+ON p.BusinessEntityID = e.BusinessEntityID;
 
 -----------------------------------
 
-WITH TestQuery 
+WITH ctehr 
 	AS 
-	(SELECT hr.BusinessEntityId, hr.NationalIdNumber, hr.JobTitle, p.FirstName, p.LastName, pp.PhoneNumber
+	 (SELECT hr.BusinessEntityId, hr.NationalIdNumber, hr.JobTitle, p.FirstName, p.LastName
 	 FROM HumanResources.Employee AS hr
-	 JOIN Person.Person AS p ON hr.BusinessEntityId = p.BusinessEntityID
-	 JOIN Person.PersonPhone AS pp ON p.BusinessEntityID = pp.BusinessEntityID
+	 JOIN Person.Person AS p ON hr.BusinessEntityId = p.BusinessEntityID),
+	 ctep
+	 AS
+	 (SELECT t.BusinessEntityId, t.NationalIdNumber, t.JobTitle, t.FirstName, t.LastName, pp.PhoneNumber
+	 FROM ctehr AS t
+	 JOIN Person.PersonPhone AS pp ON t.BusinessEntityId = pp.BusinessEntityID
 	 )
-SELECT * FROM TestQuery
+SELECT * 
+FROM ctep;
